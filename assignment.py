34764925e7b5a3e48a8e3e71b48386b1ec46e66e
isharijal21 <isharijal10@gmail.com>
2026-02-16 12:34:19 -0500

    This is the first assignment

--- assignment.py
+++ assignment.py
@@ -19,11 +19,98 @@ if __name__ == '__main__':
                 if char == " ":
                     char = "_"
                 new_list = new_list + char
-        print(new_list.title())
+        return (new_list.title())
+
+
 
 
     string = "You are my Sunshine"
-    string_tranformation(string)
+    print(string_tranformation(string))
+
+"""
+Q2. Dictionary Aggregation
+    Given a list of dictionaries representing sales transactions:
+    sales = [
+      {"product": "Pen", "amount": 10},
+      {"product": "Book", "amount": 20},
+      {"product": "Pen", "amount": 15},
+      {"product": "Pencil", "amount": 5}
+    ]
+
+    Write a Python program to calculate total sales per product and print the result as:
+    Pen: 25
+    Book: 20
+    Pencil: 5 
+"""
+
+print("========q2==============")
+sales = [
+        {"product": "Pen", "amount": 10},
+        {"product": "Book", "amount": 20},
+        {"product": "Pen", "amount": 15},
+        {"product": "Pencil", "amount": 5}
+    ]
+totals = {}
+for sell in sales:
+    product = sell["product"]
+    amount = sell["amount"]
+    if product in totals:
+        totals[product] = totals[product] + amount
+    else:
+        totals[product] = amount
+
+
+for product, total in totals.items():
+    print(product, ":", totals[product])
+
+
+print("===========q3========")
+
+"""Q3. Unique Numbers in List
+Write a Python function that takes a list of integers and returns a new list containing elements that appear exactly once.
+Example:
+Input: [4, 5, 4, 6, 7, 5, 8]
+Output: [6, 7, 8]
+"""
+
+def unique_number(lst):
+    result = []
+    for num in lst:
+        if lst.count(num) == 1:
+            result.append(num)
+    return result
+
+
+lst1 = [4, 5, 4, 6, 7, 5, 8]
+print(unique_number(lst1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
